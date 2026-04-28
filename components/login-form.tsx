8a8cb1182c6b993bb032d801227e3f34d7ff8b2e
mochNabiel <mochnabiel77@gmail.com>
2026-04-28 13:49:00 +0700

    fix: error on login with google

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -27,7 +27,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/oauth?next=/protected`,
+          redirectTo: `${window.location.origin}/auth/oauth`,
         },
       })
 
